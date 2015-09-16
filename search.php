7a10f99b2ef20388277cc59c1d2936d82e71593d
Robbi Nespu <robbi.nespu@hotmail.com>
2015-09-17 00:53:47 +0800

    A plain HTML search form using POST

---
+++ search.php
@@ -0,0 +1,19 @@
+<html>
+<title> PHP MYSQL - Search</title>
+<head>
+</head>
+
+<body>
+<form action="result.php" method="POST">
+<center><h3>Search Database</h3></center>
+<center><table>
+<tr>
+	<td>Search</td>
+	<td><input type="text" name="name" size="100"></td>
+	<td><input type="submit" name="submit"></td>
+</tr>
+</table></center>
+</form>
+</body>
+
+</html>
